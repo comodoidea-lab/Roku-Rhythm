32e86d49a0e6419bd5bbf24fc86eb9f570b03d32
comodoidea-lab <comodoidea@gmail.com>
2026-08-09 22:52:22 +0900

    fix: keep Android status bar icons readable

--- web-src/android/app/src/main/java/com/comodoidealab/rokurhythm/MainActivity.java
+++ web-src/android/app/src/main/java/com/comodoidealab/rokurhythm/MainActivity.java
@@ -3,6 +3,7 @@ package com.comodoidealab.rokurhythm;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
@@ -26,6 +27,14 @@ public class MainActivity extends BridgeActivity {
         applyEdgeToEdgeStatusBar();
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyEdgeToEdgeStatusBar();
+        }
+    }
+
     private void applyEdgeToEdgeStatusBar() {
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
@@ -36,8 +45,20 @@ public class MainActivity extends BridgeActivity {
 
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         boolean useDarkStatusBarIcons = hour >= 5 && hour < 16;
+        View decorView = getWindow().getDecorView();
+        int systemUiVisibility = decorView.getSystemUiVisibility()
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+        if (useDarkStatusBarIcons) {
+            systemUiVisibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        } else {
+            systemUiVisibility &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+        decorView.setSystemUiVisibility(systemUiVisibility);
+
         WindowInsetsControllerCompat insetsController =
-            WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+            WindowCompat.getInsetsController(getWindow(), decorView);
         insetsController.setAppearanceLightStatusBars(useDarkStatusBarIcons);
     }
 }
