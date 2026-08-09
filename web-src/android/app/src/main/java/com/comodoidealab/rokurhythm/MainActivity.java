b6580b9c49b4c78b0874bf7d7111a26971332dfa
comodoidea-lab <comodoidea@gmail.com>
2026-08-09 22:20:29 +0900

    fix: extend Android header behind status bar

--- web-src/android/app/src/main/java/com/comodoidealab/rokurhythm/MainActivity.java
+++ web-src/android/app/src/main/java/com/comodoidealab/rokurhythm/MainActivity.java
@@ -1,7 +1,15 @@
 package com.comodoidealab.rokurhythm;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends BridgeActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(NativeReceiptSharePlugin.class);
         super.onCreate(savedInstanceState);
+        applyEdgeToEdgeStatusBar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        applyEdgeToEdgeStatusBar();
+    }
+
+    private void applyEdgeToEdgeStatusBar() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setStatusBarContrastEnforced(false);
+        }
+
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        boolean useDarkStatusBarIcons = hour >= 5 && hour < 16;
+        WindowInsetsControllerCompat insetsController =
+            WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        insetsController.setAppearanceLightStatusBars(useDarkStatusBarIcons);
     }
 }
